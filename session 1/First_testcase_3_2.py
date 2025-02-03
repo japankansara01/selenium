2d4ce4149896c9d2884879872245e493c79fe75c
Japan Kansara <japan.kansara@aliansoftware.com>
2025-02-03 12:26:19 +0530

    Session 1 first commit

---
+++ session 1/First_testcase_3_2.py
@@ -0,0 +1,51 @@
+#For chrome
+# open browser
+# open url https://opensource-demo.orangehrmlive.com/
+# Enter username (Admin)
+# enter password (admin123)
+# click on login
+# capture title of the home page
+# verify title of the:OrangeHRM (Expected)
+# close browser
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+# service = Service(r"D:\japan_office\drivers\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+# driver = webdriver.Chrome(service=service)
+
+#driver = webdriver.Chrome()
+driver = webdriver.Edge()
+#driver = webdriver.Firefox()
+driver.maximize_window()
+
+driver.get("https://opensource-demo.orangehrmlive.com/")
+
+username_field = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, "username"))
+)
+username_field.send_keys("Admin")
+
+password_field = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, "password"))
+)
+password_field.send_keys("admin123")
+
+login_button = WebDriverWait(driver,10).until(
+    EC.element_to_be_clickable((By.XPATH,"//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"))
+)
+login_button.click()
+
+act_title = driver.title
+print("Page Title: ", act_title) #get title from webpage
+exp_title = "OrangeHRM"
+if act_title==exp_title:
+    print("Title Matched, Test Case passed")
+else:
+    print("Test case failed")
+
+time.sleep(5)  # used for chrome and edge
+driver.close()
---
+++ session 1/Second_testcase_3_2.py
@@ -0,0 +1,43 @@
+# open browser
+# open url https://admin-demo.nopcommerce.com/login
+# Enter email (admin@yourstore.com)
+# enter password (admin)
+# click on login
+# capture title of the home page
+# verify title of the:OrangeHRM (Expected)
+# close browser
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+#driver = webdriver.Chrome()
+driver = webdriver.Edge()
+#driver = webdriver.Firefox()
+driver.maximize_window()
+
+driver.get("https://admin-demo.nopcommerce.com/login")
+
+email_field = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "Email"))
+)
+email_field.clear()
+email_field.send_keys("admin@yourstore.com")
+
+password_field = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, "Password"))
+)
+password_field.clear()
+password_field.send_keys("admin")
+
+login_button = WebDriverWait(driver,10).until(
+    EC.element_to_be_clickable((By.XPATH,"//*[@id='main']/div/div/div/div[2]/div[1]/div/form/div[3]/button"))
+)
+login_button.click()
+
+print("Test Case Passed")
+
+time.sleep(7)  #used for chrome and edge
+driver.close()
