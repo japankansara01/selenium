15acc6fec18d306d2190263dbea3257bf5d5b258
Japan Kansara <japan.kansara@aliansoftware.com>
2025-02-10 13:13:33 +0530

    Completed Session 12

--- session 12/UploadFile_10_2.py
+++ session 12/UploadFile_10_2.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 
 obj = Service(r"D:\japan_office\drivers\chromedriver-win64\chromedriver-win64\chromedriver.exe")
 driver = webdriver.Chrome(service=obj)
-driver.maximize_window
+driver.maximize_window()
 
 driver.get("https://demo.automationtesting.in/FileUpload.html")
 
